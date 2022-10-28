e5fc620a9fe673253ca6f8be62586249ec8f1ba1
Mercad0 <abimaelmercado05@gmail.com>
2022-10-28 22:29:12 +0000

    traffic light with css & usetState hooks

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHand } from "@fortawesome/free-solid-svg-icons";
+import { faPersonArrowUpFromLine } from "@fortawesome/free-solid-svg-icons";
+import { faPersonArrowDownToLine } from "@fortawesome/free-solid-svg-icons";
+import { faLeftRight } from "@fortawesome/free-solid-svg-icons";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [selectedColor, setSelectedColor] = useState("stopRed");
+
+  return (
+    <div className="text-center">
+      <div className="pole"></div>
+      <div className="trafficLight">
+        <div
+          onClick={() => setSelectedColor("red")}
+          className={"round red " + (selectedColor === "red" ? "stopRed" : "")}
+        >
+          {" "}
+          <FontAwesomeIcon icon={faHand} />
+        </div>
+        <div
+          onClick={() => setSelectedColor("yellow")}
+          className={
+            "round yellow " + (selectedColor === "yellow" ? "slowYellow" : "")
+          }
+        >
+          {" "}
+          <FontAwesomeIcon icon={faPersonArrowDownToLine} />
+        </div>
+        {/* <<<<<<<<<<<< line from 34 to 43 are for an extra green arrow if wanted, (then go to
+        CSS to edit the height of trafficLight to 460px to fit) >>>>>>>>>>>
+        {/* <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "round green " + (selectedColor === "green" ? "goGreen" : "")
+          }
+        >
+          {" "}
+          <FontAwesomeIcon icon={faPersonArrowUpFromLine} />{" "}
+        </div> */}
+        <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "round green " + (selectedColor === "green" ? "goGreen" : "")
+          }
+        >
+          {" "}
+          <FontAwesomeIcon icon={faLeftRight} />{" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
